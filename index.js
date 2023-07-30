34fea800397dab8837bafa800cacfa504a5057c9
Yaseen YK <cbz0178@gmail.com>
2023-07-31 00:23:00 +0530

    Controller and route added

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// use express Routing
+
+app.use('/',require('./routes/router'));
+
+
+
+
+
 app.listen(port,function(){
     console.log('Server Started')
 })
